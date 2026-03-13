e52a9649df72aed9b7d599ada2384cff8ad04ec1
george980924 <pol980924@gmail.com>
2026-03-13 06:13:45 -0400

    Add files via upload

--- productos.js
+++ productos.js
@@ -1,15 +1,15 @@
 let productos = JSON.parse(localStorage.getItem("productos")) || [
 
 {
-nombre:"Aire acondicionado 12V",
-precio:"350",
-categoria:"aire",
+nombre:"Bms 4.2v 1A",
+precio:"200 c/u 170 x cant",
+categoria:"Bms",
 imagen:"https://i.imgur.com/Wk4F6QH.jpg"
 },
 
 {
-nombre:"Inversor 48V",
-precio:"120",
+nombre:"Estaño 0.8mm",
+precio:"200 c/u 180 x cant",
 categoria:"inversor",
 imagen:"https://i.imgur.com/8Km9tLL.jpg"
 }
