2d9488b391bb8aff5443c2e0cb9411fbb0c3b30d
george980924 <pol980924@gmail.com>
2026-03-19 07:57:49 -0400

    Add files via upload

--- productos.js
+++ productos.js
@@ -10,7 +10,25 @@ let productos = JSON.parse(localStorage.getItem("productos")) || [
     nombre: "Estaño 0.8mm",
     precio: 180,
     categoria: "OTROS",
-    imagen: "https://i.imgur.com/8Km9tLL.jpg",
+    imagen: "https://i.postimg.cc/2SdN0wqT/estano08mm.png",
     info: "c/u 180 x cant"
+  },
+  {
+    nombre: "Batería 12V 7Ah",
+    precio: 650,
+    categoria: "BMS",
+    imagen: "https://i.postimg.cc/4yJqL2G7/bateria12v7ah.png",
+    info: "c/u 650 x cant"
+  },
+  {
+    nombre: "Cargador 5A",
+    precio: 400,
+    categoria: "OTROS",
+    imagen: "https://i.postimg.cc/9M1Gg7bT/cargador5A.png",
+    info: "c/u 400 x cant"
   }
 ];
+
+function guardarProductos(){
+  localStorage.setItem("productos", JSON.stringify(productos));
+}
