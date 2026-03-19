8a914572091a5761b6a62afb260d5957585dce0b
george980924 <pol980924@gmail.com>
2026-03-19 06:24:21 -0400

    Add files via upload

--- productos.js
+++ productos.js
@@ -1,21 +1,20 @@
 let productos = JSON.parse(localStorage.getItem("productos")) || [
-
-{
-nombre:"Bms 4.2v 1A",
-precio:"200 c/u 170 x cant",
-categoria:"Bms",
-imagen:"https://i.imgur.com/Wk4F6QH.jpg"
-},
-
-{
-nombre:"Estaño 0.8mm",
-precio:"200 c/u 180 x cant",
-categoria:"inversor",
-imagen:"https://i.imgur.com/8Km9tLL.jpg"
-}
-
-]
+  {
+    nombre: "Bms 4.2v 1A",
+    precio: 200,
+    categoria: "BMS",
+    imagen: "https://i.imgur.com/Wk4F6QH.jpg",
+    info: "c/u 170 x cant"
+  },
+  {
+    nombre: "Estaño 0.8mm",
+    precio: 180,
+    categoria: "OTROS",
+    imagen: "https://i.imgur.com/8Km9tLL.jpg",
+    info: "c/u 180 x cant"
+  }
+];
 
 function guardarProductos(){
-localStorage.setItem("productos",JSON.stringify(productos))
+  localStorage.setItem("productos", JSON.stringify(productos));
 }
